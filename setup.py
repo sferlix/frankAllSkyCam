404cea7c7ba9c11e56b3c6f44bdd5ca3ed8d42d8
sferlix <44921687+sferlix@users.noreply.github.com>
2023-04-26 20:33:22 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="frankAllSkyCam",                 # This is the name of the package
-    version="3.0.",                         # The initial release version
-    author="Francesco Sferlazza",           # Full name of the author
-    description="AllSkyCam Package",
-    long_description=long_description,      # Long description read from the the readme file
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    # List of all python modules to be installed
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],                                      # Information to filter the project on PyPi website
-    python_requires='>=3.9',                # Minimum version requirement of the package
-    py_modules=["__main__.py"],             # Name of the python package
-    package_dir={'':'allskycam/src'},       # Directory of the source code of the package
-    install_requires=["pytz", "numpy"]                     # Install other dependencies if any
+from distutils.core import setup
+setup(
+  name = 'frankAllSkyCam',         # How you named your package folder (MyLib)
+  packages = ['frankAllSkyCam'],   # Chose the same as "name"
+  version = '3.0',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'AllSkyCamera with Raspberry Pi and Pi HQ Camera ',   # Give a short description about your library
+  author = 'Francesco Sferlazza',                   # Type in your name
+  author_email = sferlazza@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/sferlix/frankAllSkyCam',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/sferlix/frankAllSkyCam/archive/refs/tags/3.0.tar.gz',    # I explain this later on
+  keywords = ['AllSkyCamera', 'Astronomy', 'AllSky'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'pytz',
+          'numpy',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Users',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.9',
+  ],
 )
-
