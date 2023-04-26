c59879d54dc2b54476b286f1b002a9321d7a2bbe
sferlix <44921687+sferlix@users.noreply.github.com>
2023-04-27 00:17:06 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ homepath = os.path.expanduser("~")
 setup(
   name = 'frankAllSkyCam',         # How you named your package folder (MyLib)
   packages = ['frankAllSkyCam'],   # Chose the same as "name"
-  version = '3.2',      # Start with a small number and increase it with every change you make
+  version = '3.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'AllSkyCamera with Raspberry Pi and Pi HQ Camera ',   # Give a short description about your library
   author = 'Francesco Sferlazza',                   # Type in your name
   author_email = 'sferlazza@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/sferlix/frankAllSkyCam',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sferlix/frankAllSkyCam/archive/refs/tags/3.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sferlix/frankAllSkyCam/archive/refs/tags/3.3.tar.gz',    # I explain this later on
   keywords = ['AllSkyCamera', 'Astronomy', 'AllSky'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pytz',
@@ -38,9 +38,9 @@ os.mkdir(homepath+"/frankAllSkyCam/img")
 os.mkdir(homepath+"/frankAllSkyCam/logs")
 
 dest1 = homepath +'/frankAllSkyCam/config.txt'
-oldf1 = 'frankAllSkyCam/helper/config.txt'
+oldf1 = 'helper/config.txt'
 shutil.copy(oldf1, dest1)
 
 dest2 = homepath +'/frankAllSkyCam/index.html'
-oldf2 = 'frankAllSkyCam/helper/index.html'
+oldf2 = 'helper/index.html'
 shutil.copy(oldf2, dest2)
