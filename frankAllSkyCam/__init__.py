fab4b3fd80647b8ee340509f882da6f722e13e1e
sferlix <44921687+sferlix@users.noreply.github.com>
2023-04-27 22:49:36 +0200

    Add files via upload

--- frankAllSkyCam/__init__.py
+++ frankAllSkyCam/__init__.py
@@ -1,4 +1,4 @@
 # __init__.py
 
 # Version of the frankAllSkyCam
-__version__ = "3.6"
+__version__ = "3.8"
--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 import os
 import shutil
 from distutils.core import setup
+from frankAllSkyCam import crontab
 
 homepath = os.path.expanduser("~")
 setup(
   name = 'frankAllSkyCam',         # How you named your package folder (MyLib)
   packages = ['frankAllSkyCam'],   # Chose the same as "name"
-  version = '3.5',      # Start with a small number and increase it with every change you make
+  version = '3.8',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'AllSkyCamera with Raspberry Pi and Pi HQ Camera ',   # Give a short description about your library
   author = 'Francesco Sferlazza',                   # Type in your name
   author_email = 'sferlazza@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/sferlix/frankAllSkyCam',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sferlix/frankAllSkyCam/archive/refs/tags/3.5.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sferlix/frankAllSkyCam/archive/refs/tags/3.8.tar.gz',    # I explain this later on
   keywords = ['AllSkyCamera', 'Astronomy', 'AllSky'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pytz',
@@ -48,3 +49,5 @@ shutil.copy(sorg1, dest1)
 sorg2 = 'frankAllSkyCam/helper/index.html'
 dest2 = homepath +'/frankAllSkyCam/index.html'
 shutil.copy(sorg2, dest2)
+
+#s = crontab.getTimes()
