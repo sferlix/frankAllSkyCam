d01b7bcf9c385a0c1c60b183106eeebe62c29de1
sferlix <44921687+sferlix@users.noreply.github.com>
2023-06-10 19:07:22 +0200

    Add files via upload

---
+++ frankAllSkyCam/index.py
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    <meta http-equiv="refresh" content="60" />
+  </head>
+  <body>
+        <img src="/img/skycam.jpg" alt="" width="100%" />
+  </body>
+</html>
+
